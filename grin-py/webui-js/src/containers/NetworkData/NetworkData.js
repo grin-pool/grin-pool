bba7203a9eb150375a3a58505e3288605102a6f5
kylanhurt <kylan.hurt@gmail.com>
2018-11-03 22:24:49 -0700

    Update networkData to reflect both hash rates

--- grin-py/webui-js/src/containers/NetworkData/NetworkData.js
+++ grin-py/webui-js/src/containers/NetworkData/NetworkData.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
-import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts'
+import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend } from 'recharts'
 import { Row, Col, Table } from 'reactstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const C29_COLOR = '#8884d8'
+const C30_COLOR = '#cc9438'
 
 export class NetworkDataComponent extends Component {
   UNSAFE_componentWillMount () {
@@ -13,13 +16,17 @@ export class NetworkDataComponent extends Component {
   render () {
     const { networkData, latestBlock } = this.props
     const graphRateData = []
-    let maxGps
-    let minGps
+    let maxC29Gps
+    let minC29Gps
+    let maxC30Gps
+    let minC30Gps
     let maxDifficulty
     let minDifficulty
     networkData.forEach((block) => {
-      if (block.gps > maxGps || !maxGps) maxGps = block.gps
-      if (block.gps < minGps || !minGps) minGps = block.gps
+      if (block.gps[0].gps > maxC29Gps || !maxC29Gps) maxC29Gps = block.gps[0].gps
+      if (block.gps[1].gps > maxC30Gps || !maxC30Gps) maxC30Gps = block.gps[1].gps
+      if (block.gps[0].gps < minC29Gps || !minC29Gps) minC29Gps = block.gps[0].gps
+      if (block.gps[1].gps < minC30Gps || !minC30Gps) minC30Gps = block.gps[1].gps
       if (block.difficulty > maxDifficulty || !maxDifficulty) maxDifficulty = block.difficulty
       if (block.difficulty < minDifficulty || !minDifficulty) minDifficulty = block.difficulty
       graphRateData.push({
@@ -28,16 +35,18 @@ export class NetworkDataComponent extends Component {
         difficulty: block.difficulty
       })
     })
-    let latestGraphRate, latestDifficulty, latestBlockHeight
+    let c29LatestGraphRate, c30LatestGraphRate, latestDifficulty, latestBlockHeight
     if (networkData.length > 0) {
       const lastBlock = networkData[networkData.length - 1]
-      latestGraphRate = lastBlock.gps
+      c29LatestGraphRate = `C${lastBlock.gps[0].edge_bits} = ${lastBlock.gps[0].gps} gps`
+      c30LatestGraphRate = `C${lastBlock.gps[1].edge_bits} = ${lastBlock.gps[1].gps} gps`
       latestDifficulty = lastBlock.difficulty
       latestBlockHeight = lastBlock.height
     } else {
-      latestGraphRate = 0
-      latestDifficulty = 0
-      latestBlockHeight = 0
+      c29LatestGraphRate = 'n/a gps'
+      c30LatestGraphRate = 'n/a gps'
+      latestDifficulty = 'n/a'
+      latestBlockHeight = 'n/a'
     }
     const nowTimestamp = Date.now()
     const latestBlockTimeAgo = latestBlock.timestamp ? Math.floor((nowTimestamp / 1000) - latestBlock.timestamp) : ''
@@ -48,7 +57,7 @@ export class NetworkDataComponent extends Component {
             <tbody>
               <tr>
                 <td><FontAwesomeIcon style={{ marginRight: 5 }} size='lg' icon={'chart-line'} /> Graph Rate</td>
-                <td>{latestGraphRate} g/s</td>
+                <td><span style={{ color: C29_COLOR }}>{c29LatestGraphRate}</span><br /><span style={{ color: C30_COLOR }}>{c30LatestGraphRate}</span></td>
               </tr>
               <tr>
                 <td><FontAwesomeIcon style={{ marginRight: 5 }} size='lg' icon={'clock'} /> Block Found</td>
@@ -74,10 +83,14 @@ export class NetworkDataComponent extends Component {
             <LineChart data={graphRateData} >
               <XAxis dataKey='height'/>
               <Tooltip />
-              <YAxis yAxisId='left' orientation='left' domain={[Math.floor(minGps) - 1, Math.ceil(maxGps) + 1]} allowDecimals={false} />
-              <Line yAxisId='left' dataKey='gps' stroke='#8884d8' />
-              <YAxis yAxisId='right' orientation='right' domain={[minDifficulty, maxDifficulty]} stroke='#82ca9d' />
-              <Line yAxisId='right' dataKey='difficulty' stroke='#82ca9d' />
+              <Legend verticalAlign='top' height={36}/>
+              <YAxis yAxisId='left' orientation='left' stroke={C29_COLOR} domain={[minC29Gps, maxC29Gps]} allowDecimals={true} />
+              <Line dot={false} yAxisId='left' name='C29 (GPU) Graph Rate' dataKey='gps[0].gps' stroke={C29_COLOR} />
+              <YAxis yAxisId='right' orientation='right' stroke={C30_COLOR} domain={[minC30Gps, maxC30Gps]} allowDecimals={true} />
+              <Line dot={false} yAxisId='right' name='C30 (ASIC) Graph Rate' dataKey='gps[1].gps' stroke={C30_COLOR} />
+              {/* <YAxis yAxisId='right' orientation='right' domain={[minDifficulty, maxDifficulty]} stroke='#82ca9d' />
+                <Line yAxisId='right' dataKey='difficulty' stroke='#82ca9d' />
+              */}
             </LineChart>
           </ResponsiveContainer>
         </Col>
