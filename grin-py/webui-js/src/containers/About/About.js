8c06ba1a38db0cab7e333904af80334bd1a5fa4b
kylanhurt <kylan.hurt@gmail.com>
2018-11-04 18:26:30 -0800

    Add content to 'About' page and adjust styling

--- grin-py/webui-js/src/containers/About/About.js
+++ grin-py/webui-js/src/containers/About/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Col, Card, CardBody } from 'reactstrap'
+import { Col, Card, CardBody, Alert } from 'reactstrap'
 
 export class AboutComponent extends Component {
   render () {
@@ -11,7 +11,16 @@ export class AboutComponent extends Component {
               <h5 className='bold-text'>Open-Source Mining Pool for the MimbleWimble Grin Blockchain</h5>
             </div>
             <h4 className='bold-text'>About GrinPool</h4>
-            <p>Under Construction...</p>
+            <p style={{ fontWeight: 'bold' }}>GRIN is currently mining on Testnet-4, with MainNet expected to launch soon™</p>
+            <Alert color='danger' style={{ width: '50%', textAlign: 'center' }}>Please be warned that Testnet GRIN coins have no significant value.</Alert>
+            <h4>How to mine in this pool:</h4>
+            <ul>
+              <li>Supports Linux and Windows miners: mimblewimble/grin-miner and mozkomor/GrinGoldMiner</li>
+              <li>Configure your miner to connect to: stratum.mwgrinpool.com:3333</li>
+              <li>Use your GRIN wallet URL as your stratum login, no password necessary</li>
+              <li>Point your browser at <a href='http://MWGrinPool.com'>MWGrinPool.com</a></li>
+              <li>Start your miner and watch your wallet fill with GRIN</li>
+            </ul>
           </CardBody>
         </Card>
       </Col>
