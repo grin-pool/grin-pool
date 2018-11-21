24ed8055970657d7af563c87bc60fe51a18fc45c
kylanhurt <kylan.hurt@gmail.com>
2018-11-20 20:41:43 -0800

    Get initial graph up for minerSharesSubmitted

--- grin-py/webui-js/src/redux/actions/minerDataActions.js
+++ grin-py/webui-js/src/redux/actions/minerDataActions.js
@@ -7,10 +7,23 @@ export const fetchMinerData = () => async (dispatch, getState) => {
     const state = getState()
     const id = state.auth.account.id
     const latestBlockHeight = state.networkData.latestBlock.height || 0
-    const url = `${API_URL}worker/stats/${id}/${latestBlockHeight},${BLOCK_RANGE}/gps,height`
+    const url = `${API_URL}worker/stats/${id}/${latestBlockHeight},${BLOCK_RANGE}/gps,height,shares_processed`
     const minerDataResponse = await fetch(url)
     const minerData = await minerDataResponse.json()
     dispatch({ type: 'MINER_DATA', data: { historical: minerData } })
+  } catch (e) {
+    console.log('Error: ', e)
+  }
+}
+
+export const fetchMinerShares = () => async (dispatch, getState) => {
+  try {
+    const state = getState()
+    const id = state.auth.account.id
+    const url = `${API_URL}worker/stat/${id}/total_shares_processed`
+    const minerSharesResponse = await fetch(url)
+    const minerShares = await minerSharesResponse.json()
+    dispatch({ type: 'TOTAL_MINER_SHARES_SUBMITTED', data: { totalSharesSubmitted: minerShares.total_shares_processed } })
   } catch (e) {
     console.log('Error: ', e)
   }
---
+++ grin-py/webui-js/src/redux/connectors/MinerSharesConnector.js
@@ -0,0 +1,18 @@
+
+import { connect } from 'react-redux'
+import { MinerSharesComponent } from '../../containers/MinerData/MinerShares.js'
+import { fetchMinerData, fetchMinerShares } from '../actions/minerDataActions.js'
+
+const mapStateToProps = (state) => ({
+  minerData: state.minerData.historical || [],
+  totalSharesSubmitted: state.minerData.totalSharesSubmitted
+})
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchMinerData: () => dispatch(fetchMinerData()),
+    fetchMinerShares: () => dispatch(fetchMinerShares())
+  }
+}
+
+export const MinerSharesConnector = connect(mapStateToProps, mapDispatchToProps)(MinerSharesComponent)
