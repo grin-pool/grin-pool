848ded2c6a280dcff5c1a44e8fcfa36e6281933e
kylanhurt <kylan.hurt@gmail.com>
2018-12-04 21:20:13 -0800

    Impelement basic auth for fetch calls that require it

--- grin-py/webui-js/src/redux/actions/minerDataActions.js
+++ grin-py/webui-js/src/redux/actions/minerDataActions.js
@@ -1,6 +1,7 @@
 // @flow
 import { API_URL } from '../../config.js'
 import { BLOCK_RANGE } from '../../constants/dataConstants.js'
+import { basicAuth } from '../../utils/utils.js'
 
 export const fetchMinerData = () => async (dispatch, getState) => {
   try {
@@ -8,7 +9,11 @@ export const fetchMinerData = () => async (dispatch, getState) => {
     const id = state.auth.account.id
     const latestBlockHeight = state.networkData.latestBlock.height || 0
     const url = `${API_URL}worker/stats/${id}/${latestBlockHeight},${BLOCK_RANGE}/gps,height,valid_shares`
-    const minerDataResponse = await fetch(url)
+    const minerDataResponse = await fetch(url, {
+      headers: {
+        'Authorization': basicAuth(state.auth.account.token)
+      }
+    })
     const minerData = await minerDataResponse.json()
     dispatch({ type: 'MINER_DATA', data: { historical: minerData } })
   } catch (e) {
@@ -21,7 +26,11 @@ export const fetchMinerShares = () => async (dispatch, getState) => {
     const state = getState()
     const id = state.auth.account.id
     const url = `${API_URL}worker/stat/${id}/total_valid_shares`
-    const minerSharesResponse = await fetch(url)
+    const minerSharesResponse = await fetch(url, {
+      headers: {
+        'Authorization': basicAuth(state.auth.account.token)
+      }
+    })
     const minerShares = await minerSharesResponse.json()
     dispatch({ type: 'MINER_TOAL_VALID_SHARES', data: { totalSharesSubmitted: minerShares.total_valid_shares } })
   } catch (e) {
@@ -34,10 +43,27 @@ export const fetchMinerPaymentData = () => async (dispatch, getState) => {
     const state = getState()
     const id = state.auth.account.id
     const url = `${API_URL}worker/utxo/${id}`
-    const minerPaymentResponse = await fetch(url)
+    const minerPaymentResponse = await fetch(url, {
+      headers: {
+        'Authorization': basicAuth(state.auth.account.token)
+      }
+    })
     const minerPaymentData = await minerPaymentResponse.json()
     dispatch({ type: 'MINER_PAYMENT_DATA', data: minerPaymentData })
   } catch (e) {
     console.log('Error: ', e)
+  }
+}
+
+export const getLatestMinerPayment = () => async (dispatch, getState) => {
+  try {
+    const state = getState()
+    const id = state.auth.account.id
+    const url = `${API_URL}worker/payment/${id}`
+    const latestMinerPaymentResponse = await fetch(url)
+    const latestMinerPaymentData = await latestMinerPaymentResponse.json()
+    console.log('latestMinerPaymentData: ', latestMinerPaymentData)
+  } catch (e) {
+
   }
 }
