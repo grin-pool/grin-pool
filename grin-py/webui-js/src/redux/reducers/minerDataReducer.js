bcf11e1c26057af8beac9098ebd7175ca9a9a3ad
kylanhurt <kylan.hurt@gmail.com>
2018-12-05 23:04:06 -0800

    Fine tune some payment form submission stuff

--- grin-py/webui-js/src/redux/reducers/minerDataReducer.js
+++ grin-py/webui-js/src/redux/reducers/minerDataReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
 
-export const historical = (state = [], action) => {
+const historical = (state = [], action) => {
   switch (action.type) {
     case 'MINER_DATA':
       return action.data.historical
@@ -9,7 +9,7 @@ export const historical = (state = [], action) => {
   }
 }
 
-export const totalSharesSubmitted = (state = 0, action) => {
+const totalSharesSubmitted = (state = 0, action) => {
   switch (action.type) {
     case 'MINER_TOAL_VALID_SHARES':
       return action.data.totalSharesSubmitted
@@ -27,8 +27,64 @@ const paymentData = (state = {}, action) => {
   }
 }
 
+const isPaymentSettingProcessing = (state = false, action) => {
+  switch (action.type) {
+    case 'IS_PAYMENT_SETTING_PROCESSING':
+      return action.data
+    case 'UPDATE_PAYMENT_METHOD_SETTING':
+      return false
+    default:
+      return state
+  }
+}
+
+const minerPaymentTxSlate = (state = '', action) => {
+  switch (action.type) {
+    case 'MINER_PAYMENT_TX_SLATE':
+      return action.data
+    default:
+      return state
+  }
+}
+
+const isPayoutScriptLoading = (state = false, action) => {
+  switch (action.type) {
+    case 'IS_PAYOUT_SCRIPT_LOADING':
+      return action.data
+    default:
+      return state
+  }
+}
+
+const paymentFormFeedback = (state = null, action) => {
+  switch (action.type) {
+    case 'PAYMENT_FORM_FEEDBACK':
+      return action.data
+    case 'IS_PAYMENT_SETTING_PROCESSING':
+      if (action.data) {
+        return null
+      } else {
+        return {
+          message: 'There has been an issue with your submission. Please try again later.',
+          color: 'danger'
+        }
+      }
+    case 'UPDATE_PAYMENT_METHOD_SETTING':
+      return {
+        message: 'Form successfully submitted!',
+        color: 'success'
+      }
+    default:
+      return state
+  }
+}
+
 export const minerData = combineReducers({
   historical,
   totalSharesSubmitted,
-  paymentData
+  paymentData,
+  minerPaymentTxSlate,
+  isPaymentSettingProcessing,
+  isPayoutScriptLoading,
+  paymentFormFeedback
 })
