ea5cf2bd5b3814cd48f4cf6a68b5a0fd0728f3ba
kylanhurt <kylan.hurt@gmail.com>
2018-12-31 17:49:06 -0800

    Some basic SEO changes

--- grin-py/webui-js/src/containers/_layout/topbar/TopbarMenuLink.js
+++ grin-py/webui-js/src/containers/_layout/topbar/TopbarMenuLink.js
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom'
 export default class TopbarMenuLinks extends PureComponent {
   static propTypes = {
     title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
+    icon: PropTypes.string,
     path: PropTypes.string.isRequired
   };
 
   render () {
     return (
       <Link className='topbar__link' to={this.props.path}>
-        <span className={`topbar__link-icon lnr lnr-${this.props.icon}`}/>
+        {this.props.icon && <span className={`topbar__link-icon lnr lnr-${this.props.icon}`}/>}
         <p className='topbar__link-title'>{this.props.title}</p>
       </Link>
     )
--- grin-py/webui-js/src/custom/custom.js
+++ grin-py/webui-js/src/custom/custom.js
@@ -1,4 +1,4 @@
-export const colorTheme = 'theme-light'
-export const C29_COLOR = ''
-export const C31_COLOR = ''
-export const POOL_NAME = 'Mining Pool'
+export const colorTheme = 'theme-dark'
+export const C29_COLOR = '#fbf730'
+export const C31_COLOR = '#ff4861'
+export const POOL_NAME = 'MWGrinPool'
