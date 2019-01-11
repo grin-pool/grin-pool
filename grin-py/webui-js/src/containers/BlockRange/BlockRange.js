9b2f1a5e1ff015deb17749022150bc194965da81
kylanhurt <kylan.hurt@gmail.com>
2019-01-11 01:13:07 -0800

    Give block tables a range

--- grin-py/webui-js/src/containers/BlockRange/BlockRange.js
+++ grin-py/webui-js/src/containers/BlockRange/BlockRange.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react'
-import { Row, Table } from 'reactstrap'
+import { Input, Row, Table } from 'reactstrap'
 import { C29_COLOR, C31_COLOR } from '../../custom/custom.js'
 import { secondsToHms } from '../../utils/utils.js'
 
 export class BlockRange extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      range: 20
+    }
+  }
   componentDidMount () {
     const { fetchBlockRange } = this.props
-    fetchBlockRange()
+    const { range } = this.state
+    fetchBlockRange(null, range)
   }
 
   componentDidUpdate (prevProps) {
     const { latestBlockHeight, fetchBlockRange } = this.props
+    const { range } = this.state
     if (prevProps.latestBlockHeight !== latestBlockHeight) {
-      fetchBlockRange()
+      fetchBlockRange(null, range)
     }
+  }
+
+  changeRangeSize = (e) => {
+    const { fetchBlockRange } = this.props
+    const newRange = parseInt(e.target.value)
+    fetchBlockRange(null, newRange)
+    this.setState({
+      range: newRange
+    })
   }
 
   render () {
     const { recentBlocks, graphTitle } = this.props
+    const { range } = this.state
 
     const rows = []
     if (recentBlocks.length) {
-      for (let i = 1; i <= 20 && i <= recentBlocks.length - 1; i++) {
+      for (let i = 1; i <= range && i <= recentBlocks.length - 1; i++) {
         const block = recentBlocks[recentBlocks.length - i]
         const currentTimestamp = new Date().getTime()
         const paymentSecondsAgo = currentTimestamp / 1000 - block.timestamp
@@ -47,9 +65,16 @@ export class BlockRange extends Component {
 
     return (
       <Row xs={12} md={12} lg={12} xl={12}>
-        <h4 className='page-title' style={{ marginBottom: 36 }}>{graphTitle}
-        </h4>
-        <Table size='sm' responsive hover className='grinPoolStatsTable'>
+        <div style={{ width: '100%', marginBottom: '14px' }}>
+          <h4 className='page-title' style={{ marginBottom: 36, display: 'inline' }}>{graphTitle}</h4>
+          <Input type="select" name="select" onChange={this.changeRangeSize} style={{ display: 'inline', float: 'right', width: '100px' }}>
+            <option>20</option>
+            <option>100</option>
+            <option>500</option>
+          </Input>
+        </div>
+
+        <Table size='sm' responsive hover>
           <thead>
             <tr>
               <th>Height</th>
