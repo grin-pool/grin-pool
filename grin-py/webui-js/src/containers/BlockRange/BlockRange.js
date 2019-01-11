7f5466a345122334d74a4b7dca393738e839a966
kylanhurt <kylan.hurt@gmail.com>
2019-01-11 00:44:48 -0800

    Add latest network blocks table to homepage

--- grin-py/webui-js/src/containers/BlockRange/BlockRange.js
+++ grin-py/webui-js/src/containers/BlockRange/BlockRange.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
 import { Row, Table } from 'reactstrap'
-import { C29_COLOR } from '../../custom/custom.js'
+import { C29_COLOR, C31_COLOR } from '../../custom/custom.js'
 import { secondsToHms } from '../../utils/utils.js'
-export class GrinPoolRecentBlocks extends Component {
+
+export class BlockRange extends Component {
   componentDidMount () {
-    const { fetchGrinPoolRecentBlocks } = this.props
-    fetchGrinPoolRecentBlocks()
+    const { fetchBlockRange } = this.props
+    fetchBlockRange()
+  }
+
+  componentDidUpdate (prevProps) {
+    const { latestBlockHeight, fetchBlockRange } = this.props
+    if (prevProps.latestBlockHeight !== latestBlockHeight) {
+      fetchBlockRange()
+    }
   }
 
   render () {
-    const { recentBlocks } = this.props
+    const { recentBlocks, graphTitle } = this.props
 
     const rows = []
     if (recentBlocks.length) {
@@ -18,14 +26,20 @@ export class GrinPoolRecentBlocks extends Component {
         const currentTimestamp = new Date().getTime()
         const paymentSecondsAgo = currentTimestamp / 1000 - block.timestamp
         const readableTimeAgo = secondsToHms(paymentSecondsAgo)
+        const colors = {
+          C29: C29_COLOR,
+          C31: C31_COLOR
+        }
+        const rowColor = colors[`C${block.edge_bits}`]
         rows.push(
           <tr key={block.height}>
-            <td><a style={{ color: C29_COLOR }} href={`https://floonet.grinexplorer.net/block/${block.height}`} rel='noopener noreferrer' target='_blank' >{block.height}</a></td>
+            <td><a style={{ color: rowColor || C29_COLOR }} href={`https://floonet.grinexplorer.net/block/${block.height}`} rel='noopener noreferrer' target='_blank' >{block.height}</a></td>
             <td>{block.nonce}</td>
             <td>{block.hash}</td>
+            {block.edge_bits && <td style={{ color: rowColor }}>C{block.edge_bits}</td>}
             <td>{readableTimeAgo} ago</td>
             <td>{block.state}</td>
-            <td style={{ textAlign: 'right' }}>{block.actual_difficulty}</td>
+            <td style={{ textAlign: 'right' }}>{block.actual_difficulty || block.total_difficulty}</td>
           </tr>
         )
       }
@@ -33,13 +47,15 @@ export class GrinPoolRecentBlocks extends Component {
 
     return (
       <Row xs={12} md={12} lg={12} xl={12}>
-        <h4 className='page-title' style={{ marginBottom: 36 }}>Recent Blocks Found by Pool</h4>
+        <h4 className='page-title' style={{ marginBottom: 36 }}>{graphTitle}
+        </h4>
         <Table size='sm' responsive hover className='grinPoolStatsTable'>
           <thead>
             <tr>
               <th>Height</th>
               <th>Nonce</th>
               <th>Hash</th>
+              {recentBlocks[0] && recentBlocks[0].edge_bits && <th>Algo</th>}
               <th>Time</th>
               <th>State</th>
               <th style={{ textAlign: 'right' }}>Difficulty</th>
